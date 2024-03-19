fd17d89f27dd2699c245e8fd238e9b3841da1871
neddy0318 <152957560+neddy0318@users.noreply.github.com>
2024-03-19 12:38:17 +0900

    plz

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='nasdaqprophet',
-    version='0.0.1',
+    version='0.0.2',
     description='nasdaq prophet',
     author='neddy0318',
     author_email='neddy0318@gmail.com',
     url='https://github.com/neddy0318/nasdaqprophet-kr',
-    install_requires=['yfinance', 'scikit-learn' 'pandas', 'numpy', 'prophet', 'matplotlib'],
+    install_requires=['yfinance', 'scikit-learn', 'pandas', 'numpy', 'prophet', 'matplotlib'],
     packages=find_packages(exclude=[]),
     keywords=['neddy0318', 'nasdaq', 'korean won', 'prophet', 'stock'],
     python_requires='>=3.6',
